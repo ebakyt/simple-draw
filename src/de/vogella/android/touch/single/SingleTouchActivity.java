c1d1e7406c26f353d359dc1061189c38020ba4f9
Bakytbek Eshmurzaev <ebakyt@yahoo.com>
2013-08-05 14:10:05 +0300

    aciklama

---
+++ src/de/vogella/android/touch/single/SingleTouchActivity.java
@@ -0,0 +1,19 @@
+package de.vogella.android.touch.single;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SingleTouchActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(new SingleTouchEventView(this, null));
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.single_touch, menu);
+    return true;
+  }
+} 
